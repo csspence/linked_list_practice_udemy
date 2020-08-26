33321ea3f833e5594636c920fdd74e62d5c7c9cf
Crew Spence <csspence123@gmail.com>
2020-08-26 06:12:06 -0500

    Wrote node and SLL constructors with push method.

--- linked_list_practice_udemy.js
+++ linked_list_practice_udemy.js
@@ -3,3 +3,29 @@ JavaScript Algorithms and Data Structures Masterclass - Udemy
 Linked List practice
 */
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push = (val) => {
+    let node = new Node(val);
+    if(this.length === 0) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+      this.length++;
+    }
+  }
+}
